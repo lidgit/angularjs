bb7f3497aac712221327f28fb879dfe866bdbca0
Lifeng Dong <lifengd@vmware.com>
2019-01-15 22:06:05 +0800

    new creation

---
+++ conf.js
@@ -0,0 +1,26 @@
+// An example configuration file.
+exports.config = {
+  directConnect: true,
+  // Capabilities to be passed to the webdriver instance.
+  capabilities: {
+    'browserName': 'chrome' ,
+    chromeOptions: {
+//           args: ['--headless']
+    }
+  },
+
+  // Framework to use. Jasmine is recommended.
+  framework: 'jasmine',
+
+  // Spec patterns are relative to the current working directory when
+  // protractor is called.
+  specs: ['example_spec.js'],
+
+  // Options to be passed to Jasmine.
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval: 360000
+  },
+
+  allScriptsTimeout: 360000
+};
